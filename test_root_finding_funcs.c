7b787b4eef0cd906adbd196c0a581b4a0c9c3457
Marcos Botto Tornielli <mbotto@udel.edu>
2026-08-24 17:07:26 -0400

    Split up tests and rework them for clarity

--- test_root_finding_funcs.c
+++ test_root_finding_funcs.c
@@ -3,104 +3,150 @@
 
 #include "root_finding_funcs.h"
 
-int test_quadratic()
+int test_quadratic_function()
 {
-    double tol = 1e-14;
+    double tolerance = 1e-16;
 
     double test_point = 0.5;
 
-    double truth_val_function = -0.75;
-    double test_val_function  = quadratic_function(test_point);
+    double truth_value = -0.75;
+    double test_value  = quadratic_function(test_point);
 
-    double error_function = fabs(test_val_function - truth_val_function) /
-                            fabs(truth_val_function);
+    double error = fabs(test_value - truth_value) / fabs(truth_value);
 
-    double truth_val_derivative = 1;
-    double test_val_derivative  = quadratic_derivative(test_point);
-
-    double error_derivative = fabs(test_val_derivative - truth_val_derivative) /
-                              fabs(truth_val_derivative);
-
-    if (error_function < tol && error_derivative < tol)
+    if (error < tolerance)
     {
-        return 1;
+        return 0;
     }
     else
     {
-        return 0;
+        return 1;
     }
 }
 
-int test_quintic()
+int test_quadratic_derivative()
 {
-    double tol = 1e-14;
+    double tolerance = 1e-16;
 
-    double test_point = 2;
+    double test_point = 0.5;
 
-    double truth_val_function = 29;
-    double test_val_function  = quintic_function(test_point);
+    double truth_value = 1;
+    double test_value  = quadratic_derivative(test_point);
 
-    double error_function = fabs(test_val_function - truth_val_function) /
-                            fabs(truth_val_function);
+    double error = fabs(test_value - truth_value) / fabs(truth_value);
 
-    double truth_val_derivative = 79;
-    double test_val_derivative  = quintic_derivative(test_point);
-
-    double error_derivative = fabs(test_val_derivative - truth_val_derivative) /
-                              fabs(truth_val_derivative);
-
-    if (error_function < tol && error_derivative < tol)
+    if (error < tolerance)
     {
-        return 1;
+        return 0;
     }
     else
     {
+        return 1;
+    }
+}
+
+int test_quintic_function()
+{
+    double tolerance = 1e-16;
+
+    double test_point = 2;
+
+    double truth_value = 29;
+    double test_value  = quintic_function(test_point);
+
+    double error = fabs(test_value - truth_value) / fabs(truth_value);
+
+    if (error < tolerance)
+    {
         return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int test_quintic_derivative()
+{
+    double tolerance = 1e-16;
+
+    double test_point = 2;
+
+    double truth_value = 79;
+    double test_value  = quintic_derivative(test_point);
+
+    double error = fabs(test_value - truth_value) / fabs(truth_value);
+
+    if (error < tolerance)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
     }
 }
 
 int test_find_root()
 {
-    double tol = 1e-14;
+    double tolerance = 1e-16;
 
+    // The quadratic we're using for this test has roots at -1 and 1
     double truth_val = 1;
 
+    // If we start at 0.5, we should get the root at 1
     double guess = 0.5;
     int verbose = 0;
+
     double test_val = find_root(quadratic_function, quadratic_derivative,
                                 guess, verbose);
 
     double error = fabs(test_val - truth_val) / fabs(truth_val);
 
-    if (error < tol)
+    if (error < tolerance)
     {
-        return 1;
+        return 0;
     }
     else
     {
-        return 0;
+        return 1;
+    }
+}
+
+void run_test(int (*test)(), int test_index, int num_tests)
+{
+    printf("Test %i/%i... ", test_index, num_tests);
+
+    if (test() == 0)
+    {
+        printf("Passed.\n");
+    }
+    else
+    {
+        printf("Failed.\n");
     }
 }
 
 int main()
 {
-    printf("\nTest 1/3... ");
-    if (test_quadratic())
-        printf("Passed.\n");
-    else
-        printf("Failed.\n");
+    int num_tests = 5;
 
-    printf("Test 2/3... ");
-    if (test_quintic())
-        printf("Passed.\n");
-    else
-        printf("Failed.\n");
+    int (*tests[])() = {
+        test_quadratic_function,
+        test_quadratic_derivative,
+        test_quintic_function,
+        test_quintic_derivative,
+        test_find_root
+    };
 
-    printf("Test 3/3... ");
-    if (test_find_root())
-        printf("Passed.\n\n");
-    else
-        printf("Failed.\n\n");
+    printf("\n");
+
+    for (int i = 0; i < num_tests; i++)
+    {
+        run_test(tests[i], i + 1, num_tests);
+    }
+
+    printf("\n");
 
     return 0;
 }
